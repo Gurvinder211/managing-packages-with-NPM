c4a033d19d16dd41723e405705935b07dc51ef21
Gurvinder Singh Mudhar <gsm21102004@gmail.com>
2021-10-03 00:31:17 +0530

    Add a route to send hello express

--- server.js
+++ server.js
@@ -54,6 +54,9 @@ if (!process.env.DISABLE_XORIGIN) {
 //       .send(err.message || 'SERVER ERROR');
 //   }  
 // })
+app.get("/", function(req, res) {
+  res.send("Hello Express");
+});
 
 //Listen on port set in environment variable or default to 3000
 var port = process.env.PORT || 3000;
