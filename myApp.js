32b2a9d4c331f0a6bda4046bc3e645b1285096b5
Gurvinder Singh Mudhar <gsm21102004@gmail.com>
2021-10-04 00:39:42 +0530

    organize application function to myApp.js

--- myApp.js
+++ myApp.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var bGround = require('fcc-express-bground');
 var app = express();
+
 bGround.log("Hello World");
-console.log("Hello World")
+console.log("Hello World");
+
+app.use(express.static(__dirname + "/public"));
+app.use('/public', express.static(__dirname + "/public"));
+
+
 module.exports = app;
